98b554e763f04ecd275054c455051bcaa20153ee
AviweP <prudencedonovanmalopi@gmail.com>
2023-07-14 16:11:46 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 /**
-<<<<<<< HEAD
- * main - Aprogram that print a line with puts function
- * Return: 0
-=======
- * main - Program that print a line with betty style
+ * main - Entry point
  * Return: 0 (Success)
->>>>>>> 74e498acedfc430b4719fcb935c43818c5b43332
  */
 int main(void)
 {
-puts("/"Programming is like building a multilingual puzzle");
-return (0);
+puts("/") Programming is like building a multilingual puzzle""));
+return (0);;
 }
-<<<<<<< HEAD
 
-=======
->>>>>>> 74e498acedfc430b4719fcb935c43818c5b43332
