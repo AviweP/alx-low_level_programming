7c7195c0d04d8b526e255e4c89d1d836e2770f21
AviweP <prudencedonovanmalopi@gmail.com>
2023-07-13 18:58:36 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main -Entry point 
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	put("\"Programming is like building a multingual puzzle");
+	return (0);
+}
