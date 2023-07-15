99fbe2c94e6f5f29dcaed47b998d1ead50fc24a4
AviweP <prudencedonovanmalopi@gmail.com>
2023-07-14 18:21:55 -0700

    alphabetic

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,30 +1,18 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: 'get the last degit of a number'
+ * Description: 'Print alphabets lowercase'
  * Return: Always 0
  */
 int main(void)
 {
-	int n;
-	int ld;
+	int n = 97;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	ld = n % 10;
-	if (ld > 5)
+	while (n <= 122)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
+		putchar(n);
+		n++;
 	}
-	else if (ld == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, ld);
-	}
-	else
-	{
-		printf("Last digit of %d is %d and is is less than 6 and not 0\n", n, ld);
-	}
+	putchar('\n');
 	return (0);
 }
