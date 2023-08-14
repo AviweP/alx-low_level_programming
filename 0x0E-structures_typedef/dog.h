e35b4e389d55fe62f10cdcc1e23f6abb20e6527e
AviweP <prudencedonovanmalopi@gmail.com>
2023-08-14 16:10:39 -0700

    my dog name

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a struct that create a dog information
+ *
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog 
+ *
+ * Descriptin: struct with alx
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
