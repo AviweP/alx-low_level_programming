f31048eaf110067124f09f0a21bd02ccc50320b5
AviweP <prudencedonovanmalopi@gmai.com>
2023-08-29 19:22:28 -0700

    sum listint

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -9,12 +9,11 @@
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
-	listint_t *temp = head;
 
 	while (head != NULL)
 	{
 		sum = head->n;
-		temp = temp->next;
+		head = head->next;
 	}
 
 	return (sum);
